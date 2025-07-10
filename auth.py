129066c7d710a0a571de77833f32b253eae90c08
DSciAILab <167185568+DSciAILab@users.noreply.github.com>
2025-07-10 11:27:10 +0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
 def check_authentication():
-    """
-    Verifica se o usuário está logado.
-    Se não estiver, redireciona para a página de login.
-    """
-    # Acessa o st.session_state para verificar a flag 'user_confirmed'
-    # Usamos .get() para evitar erros caso a chave ainda não exista
     if not st.session_state.get("user_confirmed", False):
-        # Se não estiver confirmada, redireciona para a página de login e para a execução.
         st.switch_page("1_Login.py")

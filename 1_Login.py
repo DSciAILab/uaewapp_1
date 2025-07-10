48752b89f62db4af01828f58e9f20db6277e1170
DSciAILab <167185568+DSciAILab@users.noreply.github.com>
2025-07-10 12:02:33 +0400

    Update 1_Login.py

--- 1_Login.py
+++ 1_Login.py
@@ -1,20 +1,15 @@
 import streamlit as st
-from utils import get_valid_user_info, display_user_sidebar
+from utils import get_valid_user_info
 
-# Página de Login não precisa da sidebar de usuário
 st.set_page_config(page_title="UAEW Login", layout="centered")
+
+if st.session_state.get("user_confirmed"):
+    st.switch_page("pages/2_Dashboard.py")
 
 st.title("UAEW | Controle de Acesso")
 st.markdown("Por favor, faça o login para continuar.")
 
-if 'user_confirmed' not in st.session_state:
-    st.session_state['user_confirmed'] = False
-
-# Se o usuário já estiver logado e tentar acessar a página de login, redirecione-o
-if st.session_state.user_confirmed:
-    st.switch_page("pages/2_Dashboard.py")
-
-user_id_input = st.text_input("Digite seu PS ou Nome de Usuário", key="login_user_input")
+user_id_input = st.text_input("PS Number ou Nome de Usuário", key="login_user_input")
 
 if st.button("Login", type="primary", use_container_width=True):
     if user_id_input:
@@ -22,11 +17,8 @@ if st.button("Login", type="primary", use_container_width=True):
         if user_info:
             st.session_state['user_confirmed'] = True
             st.session_state['current_user_name'] = str(user_info.get("USER", user_id_input)).strip()
-            st.session_state['current_user_ps_id_internal'] = str(user_info.get("PS", "")).strip()
-            st.session_state['current_user_image_url'] = str(user_info.get("USER_IMAGE", "")).strip()
+            # ... Armazene outras informações do usuário se precisar
             st.toast(f"Bem-vindo, {st.session_state['current_user_name']}!", icon="👋")
             st.switch_page("pages/2_Dashboard.py")
         else:
             st.error("Usuário não encontrado.", icon="🚨")
-    else:
-        st.warning("Por favor, insira seus dados.", icon="⚠️")
